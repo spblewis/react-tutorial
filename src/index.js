145455760457a6a91c4cedada02b6c19000986b8
spblewis <spblewis@gmail.com>
2021-05-21 18:04:37 -0500

    State is lifted...

--- src/index.js
+++ src/index.js
@@ -7,18 +7,15 @@ import './index.css'
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: null,
-    };
   }
 
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})}
+          onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -32,9 +29,18 @@ class Square extends React.Component {
       }
     }
 
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
 
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />);
     }
   
     render() {
