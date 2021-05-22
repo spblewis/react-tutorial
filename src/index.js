21fe829010b1d40e7a8612bc53744fd567221f39
spblewis <spblewis@gmail.com>
2021-05-22 10:35:32 -0500

    Square becomes a functional component

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ import './index.css'
 
 
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-    render() {
+function Square(props) {
       return (
         <button 
           className="square" 
-          onClick={() => this.props.onClick()}
+          onClick={props.onClick}
         >
-          {this.props.value}
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
